ca70d42541d4a17d5015df011a92b4e8db18a18a
FloatingGhost <hannah@coffee-and-dreams.uk>
2022-12-16 11:18:14 +0000

    mix format

--- test/pleroma/web/akkoma_api/metrics_controller_test.exs
+++ test/pleroma/web/akkoma_api/metrics_controller_test.exs
@@ -4,15 +4,18 @@ defmodule Pleroma.Web.AkkomaAPI.MetricsControllerTest do
   describe "GET /api/v1/akkoma/metrics" do
     test "should return metrics when the user has admin:metrics" do
       %{conn: conn} = oauth_access(["admin:metrics"])
-      resp = conn
-      |> get("/api/v1/akkoma/metrics")
-      |> text_response(200)
+
+      resp =
+        conn
+        |> get("/api/v1/akkoma/metrics")
+        |> text_response(200)
 
       assert resp =~ "# HELP"
     end
 
     test "should not allow users that do not have the admin:metrics scope" do
       %{conn: conn} = oauth_access(["read:metrics"])
+
       conn
       |> get("/api/v1/akkoma/metrics")
       |> json_response(403)
@@ -21,6 +24,7 @@ defmodule Pleroma.Web.AkkomaAPI.MetricsControllerTest do
     test "should be disabled by export_prometheus_metrics" do
       clear_config([:instance, :export_prometheus_metrics], false)
       %{conn: conn} = oauth_access(["admin:metrics"])
+
       conn
       |> get("/api/v1/akkoma/metrics")
       |> response(404)
