7fb9960ccddfc078be28c1b2716eff07a90fa7b3
FloatingGhost <hannah@coffee-and-dreams.uk>
2023-05-26 11:46:18 +0100

    Add CSP to mediaproxy links

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pleroma.Mixfile do
   def project do
     [
       app: :pleroma,
-      version: version("3.9.0"),
+      version: version("3.9.1"),
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
