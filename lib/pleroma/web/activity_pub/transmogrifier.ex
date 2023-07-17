33e7ae7637236b859bfabc9016a86e4bbe1e0632
FloatingGhost <hannah@coffee-and-dreams.uk>
2023-07-17 20:03:31 +0100

    Allow nil attachments

--- lib/pleroma/web/activity_pub/transmogrifier.ex
+++ lib/pleroma/web/activity_pub/transmogrifier.ex
@@ -919,9 +919,13 @@ defmodule Pleroma.Web.ActivityPub.Transmogrifier do
   end
 
   def prepare_attachments(object) do
+    attachments = case Map.get(object, "attachment", []) do
+      [_ | _] = list -> list
+      _ -> []
+    end
+    
     attachments =
-      object
-      |> Map.get("attachment", [])
+      attachments
       |> Enum.map(fn data ->
         [%{"mediaType" => media_type, "href" => href} = url | _] = data["url"]
 
