c193b4d507d5dd4a7be545e4368e01f2b692d01f
FloatingGhost <hannah@coffee-and-dreams.uk>
2023-08-06 16:20:46 +0100

    Remove frankly awful config file test

--- test/support/matchers/list.ex
+++ test/support/matchers/list.ex
@@ -1,6 +1,6 @@
 defmodule Pleroma.Test.Matchers.List do
   import ExUnit.Assertions
- 
+
   def assert_unordered_list_equal(list_a, list_b) when is_list(list_a) and is_list(list_b) do
     assert Enum.sort(list_a) == Enum.sort(list_b)
   end
