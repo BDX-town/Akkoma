5c164028cf7e45811ddcf8b9cfd3dcab56717827
FloatingGhost <hannah@coffee-and-dreams.uk>
2023-08-16 23:11:36 +0100

    ensure ap_enabled true if coming back pleroma

--- priv/repo/optional_migrations/pleroma_develop_rollbacks/20230504173400_remove_user_ap_enabled.exs
+++ priv/repo/optional_migrations/pleroma_develop_rollbacks/20230504173400_remove_user_ap_enabled.exs
@@ -1,9 +1,15 @@
 defmodule Pleroma.Repo.Migrations.RemoveUserApEnabled do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:users) do
-      remove(:ap_enabled, :boolean, default: false, null: false)
+      remove_if_exists(:ap_enabled, :boolean)
+    end
+  end
+
+  def down do
+    alter table(:users) do
+      add_if_not_exists(:ap_enabled, :boolean, default: true, null: false)
     end
   end
 end
