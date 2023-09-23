3286c6f7699caccea25ebdf28cd1626454ad2ce0
Clovis <clovis@bdx.town>
2023-09-23 17:37:05 +0200

    Handle rich media error without crash

--- lib/pleroma/web/rich_media/parser.ex
+++ lib/pleroma/web/rich_media/parser.ex
@@ -131,13 +131,17 @@ defmodule Pleroma.Web.RichMedia.Parser do
   end
 
   def parse_url(url) do
-    with {:ok, %Tesla.Env{body: html}} <- Pleroma.Web.RichMedia.Helpers.rich_media_get(url),
-         {:ok, html} <- Floki.parse_document(html) do
-      html
-      |> maybe_parse()
-      |> Map.put("url", url)
-      |> clean_parsed_data()
-      |> check_parsed_data()
+    try do
+      with {:ok, %Tesla.Env{body: html}} <- Pleroma.Web.RichMedia.Helpers.rich_media_get(url),
+          {:ok, html} <- Floki.parse_document(html) do
+        html
+        |> maybe_parse()
+        |> Map.put("url", url)
+        |> clean_parsed_data()
+        |> check_parsed_data()
+      end
+    rescue
+      e -> Logger.warning("Fail while fetching rich media for #{url}: #{Exception.format(:error, e, __STACKTRACE__)}")
     end
   end
 
