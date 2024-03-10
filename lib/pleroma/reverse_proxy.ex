11ae8344eb62fea51b9fc26a063406f2afe253ac
Oneric <oneric@oneric.stub>
2024-03-10 18:57:19 +0000

    Sanitise Content-Type of media proxy URLs
    Just as with uploads and emoji before, this can otherwise be used
    to place counterfeit AP objects or other malicious payloads.
    In this case, even if we never assign a priviliged type to content,
    the remote server can and until now we just mimcked whatever it told us.
    
    Preview URLs already handle only specific, safe content types
    and redirect to the external host for all else; thus no additional
    sanitisiation is needed for them.
    
    Non-previews are all delegated to the modified ReverseProxy module.
    It already has consolidated logic for building response headers
    making it easy to slip in sanitisation.
    
    Although proxy urls are prefixed by a MAC built from a server secret,
    attackers can still achieve a perfect id match when they are able to
    change the contents of the pointed to URL. After sending an posts
    containing an attachment at a controlled destination, the proxy URL can
    be read back and inserted into the payload. After injection of
    counterfeits in the target server the content can again be changed
    to something innocuous lessening chance of detection.

--- lib/pleroma/reverse_proxy.ex
+++ lib/pleroma/reverse_proxy.ex
@@ -17,6 +17,8 @@ defmodule Pleroma.ReverseProxy do
   @failed_request_ttl :timer.seconds(60)
   @methods ~w(GET HEAD)
 
+  @allowed_mime_types Pleroma.Config.get([Pleroma.Upload, :allowed_mime_types], [])
+
   @cachex Pleroma.Config.get([:cachex, :provider], Cachex)
 
   def max_read_duration_default, do: @max_read_duration
@@ -253,6 +255,7 @@ defmodule Pleroma.ReverseProxy do
     headers
     |> Enum.filter(fn {k, _} -> k in @keep_resp_headers end)
     |> build_resp_cache_headers(opts)
+    |> sanitise_content_type()
     |> build_resp_content_disposition_header(opts)
     |> build_csp_headers()
     |> Keyword.merge(Keyword.get(opts, :resp_headers, []))
@@ -280,6 +283,21 @@ defmodule Pleroma.ReverseProxy do
           {"cache-control", @default_cache_control_header}
         )
     end
+  end
+
+  defp sanitise_content_type(headers) do
+    original_ct = get_content_type(headers)
+
+    safe_ct =
+      Pleroma.Web.Plugs.Utils.get_safe_mime_type(
+        %{allowed_mime_types: @allowed_mime_types},
+        original_ct
+      )
+
+    [
+      {"content-type", safe_ct}
+      | Enum.filter(headers, fn {k, _v} -> k != "content-type" end)
+    ]
   end
 
   defp build_resp_content_disposition_header(headers, opts) do
