8e789c62365a1c11eb0f0c1b51f2b67710f99fee
Floatingghost <hannah@coffee-and-dreams.uk>
2025-03-02 13:07:03 +0000

    1.14.1 min version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pleroma.Mixfile do
     [
       app: :pleroma,
       version: version("3.15.1"),
-      elixir: "~> 1.15",
+      elixir: "~> 1.14.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       elixirc_options: [warnings_as_errors: warnings_as_errors()],
