BDX-town/Akkoma

commit 8f322456a0eb3416fb8f5cf9c720c618c2c16f72

Allow unsigned fetches of a user's public key

lib/pleroma/web/activity_pub/activity_pub_controller.ex
@@ -60,12 +60,44 @@ defmodule Pleroma.Web.ActivityPub.ActivityPubController do
     end
   end
 
-  def user(conn, %{"nickname" => nickname}) do
+
+  @doc """
+  Render the user's AP data
+  WARNING: we cannot actually check if the request has a fragment! so let's play defensively
+  - IF we have a valid signature, serve full user
+  - IF we do not, and authorized_fetch_mode is enabled, serve the key only
+  - OTHERWISE, serve the full actor (since we don't need to worry about the signature)
+  """
+  def user(%{assigns: %{valid_signature: true}} = conn, params) do
+    render_full_user(conn, params)
+  end
+
+  def user(conn, params) do
+    if Pleroma.Config.get([:activitypub, :authorized_fetch_mode], false) do
+      render_key_only_user(conn, params)
+    else
+      render_full_user(conn, params)
+    end
+  end
+
+  defp render_full_user(conn, %{"nickname" => nickname}) do
     with %User{local: true} = user <- User.get_cached_by_nickname(nickname) do
       conn
       |> put_resp_content_type("application/activity+json")
       |> put_view(UserView)
       |> render("user.json", %{user: user})
+    else
+      nil -> {:error, :not_found}
+      %{local: false} -> {:error, :not_found}
+    end
+  end
+
+  def render_key_only_user(conn, %{"nickname" => nickname}) do
+    with %User{local: true} = user <- User.get_cached_by_nickname(nickname) do
+      conn
+      |> put_resp_content_type("application/activity+json")
+      |> put_view(UserView)
+      |> render("keys.json", %{user: user})
     else
       nil -> {:error, :not_found}
       %{local: false} -> {:error, :not_found}

lib/pleroma/web/activity_pub/views/user_view.ex
@@ -32,7 +32,7 @@ defmodule Pleroma.Web.ActivityPub.UserView do
   def render("endpoints.json", _), do: %{}
 
   def render("service.json", %{user: user}) do
-    public_key = User.SigningKey.public_key_pem(user)
+    {:ok, public_key} = User.SigningKey.public_key_pem(user)
 
     endpoints = render("endpoints.json", %{user: user})
 
@@ -67,7 +67,7 @@ defmodule Pleroma.Web.ActivityPub.UserView do
     do: render("service.json", %{user: user}) |> Map.put("preferredUsername", user.nickname)
 
   def render("user.json", %{user: user}) do
-    public_key = User.SigningKey.public_key_pem(user)
+    {:ok, public_key} = User.SigningKey.public_key_pem(user)
     user = User.sanitize_html(user)
 
     endpoints = render("endpoints.json", %{user: user})
@@ -112,7 +112,7 @@ defmodule Pleroma.Web.ActivityPub.UserView do
   end
 
   def render("keys.json", %{user: user}) do
-    public_key = User.SigningKey.public_key_pem(user)
+    {:ok, public_key} = User.SigningKey.public_key_pem(user)
 
     %{
       "id" => user.ap_id,

lib/pleroma/user/signing_key.ex
@@ -23,8 +23,12 @@ defmodule Pleroma.User.SigningKey do
     |> Repo.preload(:signing_key)
   end
 
-  def key_id_of_local_user(%User{local: true, signing_key: %__MODULE__{key_id: key_id}}),
-    do: key_id
+  def key_id_of_local_user(%User{local: true} = user) do
+    case Repo.preload(user, :signing_key) do
+      %User{signing_key: %__MODULE__{key_id: key_id}} -> key_id
+      _ -> nil
+    end
+  end
 
   @spec remote_changeset(__MODULE__, map) :: Changeset.t()
   def remote_changeset(%__MODULE__{} = signing_key, attrs) do
@@ -119,9 +123,16 @@ defmodule Pleroma.User.SigningKey do
 
   def public_key(_), do: {:error, "key not found"}
 
-  def public_key_pem(%User{signing_key: %__MODULE__{public_key: public_key_pem}}),
-    do: public_key_pem
+  def public_key_pem(%User{} = user) do
+    case Repo.preload(user, :signing_key) do
+      %User{signing_key: %__MODULE__{public_key: public_key_pem}} -> {:ok, public_key_pem}
+      _ -> {:error, "key not found"}
+    end
+  end
 
+  def public_key_pem(e) do
+    {:error, "key not found"}
+  end
   @spec private_key(User.t()) :: {:ok, binary()} | {:error, String.t()}
   @doc """
   Given a user, return the private key for that user in binary format.
@@ -146,7 +157,8 @@ defmodule Pleroma.User.SigningKey do
   So if we're rejected, we should probably just give up.
   """
   def fetch_remote_key(key_id) do
-    resp = HTTP.Backoff.get(key_id)
+    # we should probably sign this, just in case
+    resp = Pleroma.Object.Fetcher.get_object(key_id)
 
     case handle_signature_response(resp) do
       {:ok, ap_id, public_key_pem} ->

lib/pleroma/web/plugs/ensure_user_public_key_plug.ex
@@ -1,0 +1,33 @@
+defmodule Pleroma.Web.Plugs.EnsureUserPublicKeyPlug do
+  @moduledoc """
+  This plug will attempt to pull in a user's public key if we do not have it.
+  We _should_ be able to request the URL from the key URL...
+  """
+
+  import Plug.Conn
+
+  alias Pleroma.User
+
+  def init(options), do: options
+
+  def call(conn, _opts) do
+    key_id = key_id_from_conn(conn)
+    unless is_nil(key_id) do
+      SigningKey.fetch_remote_key(key_id)
+      # now we SHOULD have the user that owns the key locally. maybe.
+      # if we don't, we'll error out when we try to validate.
+    end
+
+    conn
+  end
+
+  defp key_id_from_conn(conn) do
+    case HTTPSignatures.signature_for_conn(conn) do
+      %{"keyId" => key_id} when is_binary(key_id) ->
+        key_id
+
+      _ ->
+        nil
+    end
+  end
+end

lib/pleroma/web/router.ex
@@ -144,7 +144,14 @@ defmodule Pleroma.Web.Router do
     })
   end
 
+  pipeline :optional_http_signature do
+    plug(Pleroma.Web.Plugs.EnsureUserPublicKeyPlug)
+    plug(Pleroma.Web.Plugs.HTTPSignaturePlug)
+    plug(Pleroma.Web.Plugs.MappedSignatureToIdentityPlug)
+  end
+
   pipeline :http_signature do
+    plug(Pleroma.Web.Plugs.EnsureUserPublicKeyPlug)
     plug(Pleroma.Web.Plugs.HTTPSignaturePlug)
     plug(Pleroma.Web.Plugs.MappedSignatureToIdentityPlug)
     plug(Pleroma.Web.Plugs.EnsureHTTPSignaturePlug)
@@ -745,7 +752,7 @@ defmodule Pleroma.Web.Router do
   scope "/", Pleroma.Web do
     # Note: html format is supported only if static FE is enabled
     # Note: http signature is only considered for json requests (no auth for non-json requests)
-    pipe_through([:accepts_html_xml_json, :http_signature, :static_fe])
+    pipe_through([:accepts_html_xml_json, :optional_http_signature, :static_fe])
 
     # Note: returns user _profile_ for json requests, redirects to user _feed_ for non-json ones
     get("/users/:nickname", Feed.UserController, :feed_redirect, as: :user_feed)

commit f754feb67dbe382692eff851c785d567a9affd83

federation: include required actor fields in minimal user fallback
ActivityPub spec demands each actor has at least an inbox and outbox.
Furthermore, the current representation wouldn’t even be accepted by
ourselves, since our processing requires objects to be flagged with a
sensible type else we don't know what to do with it.
Including the nickname is just a peemptive measure.

There were no reports of this causing problems in real-world deployments
and at least for federation with other Akkoma instances we should have
never run into this, since we _always_ expose the full representation of
the instance actor and atm also always use the latter for fetching
remote content (which prevents us from fetching followers-only content).
Nonetheless, serving something which violates spec and we wouldn’t even
accept ourselves seems obviously bad, so fix it and add tests to prevent
this from reoccuring.

Fixes bug introduced in 8f322456a0eb3416fb8f5cf9c720c618c2c16f72

lib/pleroma/web/activity_pub/activity_pub_controller.ex
@@ -62,7 +62,7 @@ defmodule Pleroma.Web.ActivityPub.ActivityPubController do
   Render the user's AP data
   WARNING: we cannot actually check if the request has a fragment! so let's play defensively
   - IF we have a valid signature, serve full user
-  - IF we do not, and authorized_fetch_mode is enabled, serve the key only
+  - IF we do not, and authorized_fetch_mode is enabled, serve only the key and bare minimum info
   - OTHERWISE, serve the full actor (since we don't need to worry about the signature)
   """
   def user(%{assigns: %{valid_signature: true}} = conn, params) do
@@ -94,7 +94,7 @@ defmodule Pleroma.Web.ActivityPub.ActivityPubController do
       conn
       |> put_resp_content_type("application/activity+json")
       |> put_view(UserView)
-      |> render("keys.json", %{user: user})
+      |> render("stripped_user.json", %{user: user})
     else
       nil -> {:error, :not_found}
       %{local: false} -> {:error, :not_found}

lib/pleroma/web/activity_pub/views/user_view.ex
@@ -113,7 +113,10 @@ defmodule Pleroma.Web.ActivityPub.UserView do
     |> Map.merge(Utils.make_json_ld_header())
   end
 
-  def render("keys.json", %{user: user}) do
+  # For unauthenticated requests when authfetch is enabled.
+  # Still serve the key and the bare minimum of required fields
+  # to avoid being stuck in an infinite "cannot verify" loop with remotes.
+  def render("stripped_user.json", %{user: user}) do
     {:ok, public_key} = User.SigningKey.public_key_pem(user)
 
     %{
@@ -122,7 +125,14 @@ defmodule Pleroma.Web.ActivityPub.UserView do
         "id" => User.SigningKey.key_id_of_local_user(user),
         "owner" => user.ap_id,
         "publicKeyPem" => public_key
-      }
+      },
+      # REQUIRED fields per AP spec
+      "inbox" => "#{user.ap_id}/inbox",
+      "outbox" => "#{user.ap_id}/outbox",
+      # allow type-based processing
+      "type" => user.actor_type,
+      # since Mastodon requires a WebFinger address for all users, this seems like a good idea
+      "preferredUsername" => user.nickname
     }
     |> Map.merge(Utils.make_json_ld_header())
   end

test/pleroma/web/activity_pub/activity_pub_controller_test.exs
@@ -133,6 +133,28 @@ defmodule Pleroma.Web.ActivityPub.ActivityPubControllerTest do
       assert json_response(conn, 200) == UserView.render("user.json", %{user: user})
     end
 
+    test "it returns a minimal json representation of the user when autfetch is enabled but no signature",
+         %{
+           conn: conn
+         } do
+      clear_config([:activitypub, :authorized_fetch_mode], true)
+
+      user = insert(:user) |> with_signing_key()
+
+      conn =
+        conn
+        |> assign(:valid_signature, false)
+        |> put_req_header(
+          "accept",
+          "application/ld+json; profile=\"https://www.w3.org/ns/activitystreams\""
+        )
+        |> get("/users/#{user.nickname}")
+
+      user = User.get_cached_by_id(user.id)
+
+      assert json_response(conn, 200) == UserView.render("stripped_user.json", %{user: user})
+    end
+
     test "it returns 404 for remote users", %{
       conn: conn
     } do

test/pleroma/web/activity_pub/views/user_view_test.exs
@@ -7,8 +7,31 @@ defmodule Pleroma.Web.ActivityPub.UserViewTest do
   import Pleroma.Factory
 
   alias Pleroma.User
+  alias Pleroma.Web.ActivityPub.ObjectValidators.UserValidator
   alias Pleroma.Web.ActivityPub.UserView
   alias Pleroma.Web.CommonAPI
+
+  test "Renders a user such that we accept it ourselves" do
+    user =
+      insert(:user)
+      |> with_signing_key()
+
+    representation = UserView.render("user.json", %{user: user})
+    validation_res = UserValidator.validate(representation, [])
+
+    assert match?({:ok, _user, _meta}, validation_res)
+  end
+
+  test "Renders a minimal user such that we accept it ourselves" do
+    user =
+      insert(:user)
+      |> with_signing_key()
+
+    representation = UserView.render("stripped_user.json", %{user: user})
+    validation_res = UserValidator.validate(representation, [])
+
+    assert match?({:ok, _user, _meta}, validation_res)
+  end
 
   test "Renders a user, including the public key" do
     user =